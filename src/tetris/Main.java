6fed3c8c085ae0f591bf9360910efd81458cb63f
todaybrian <todaybryan@gmail.com>
2022-06-01 17:44:33 -0400

    Add runMe.bat

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -1,9 +1,12 @@
 
 package tetris;
 
+import tetris.util.Assets;
+
 public class Main {
     public static GameFrame instance;
     public static void main(String[] args){
-
+        instance = new GameFrame();
+        System.out.println(Assets.LOGO_FILE);
     }
 }
