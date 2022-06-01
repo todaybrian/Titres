4a9f8b821b09985c29ef6b2a1fc8838626f02496
todaybrian <todaybryan@gmail.com>
2022-06-01 18:11:17 -0400

    Decouple physics and render fps. Class reorganization required.

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -1,12 +1,9 @@
-
 package tetris;
 
-import tetris.util.Assets;
 
 public class Main {
     public static GameFrame instance;
     public static void main(String[] args){
         instance = new GameFrame();
-        System.out.println(Assets.LOGO_FILE);
     }
 }
