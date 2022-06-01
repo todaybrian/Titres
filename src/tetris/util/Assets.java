18c6349cb8a3d0a3dea06093a5e0eefa4f4ae3c7
aaronz13248 <aaronz13248@gmail.com>
2022-06-01 18:22:49 -0400

    add music to assets

--- src/tetris/util/Assets.java
+++ src/tetris/util/Assets.java
@@ -7,4 +7,7 @@ package tetris.util;
 
 public class Assets {
     public static final String LOGO_FILE = "src/assets/logo.png";
+    public static final String NIGHT_SNOW = "src/assets/NightSnow.wav";
+    public static final String VREMYA = "src/assets/Vremya.wav";
+    public static final String VIRTUAL_LIGHT = "src/assets/VirtualLight.wav";
 }
