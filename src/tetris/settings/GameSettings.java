594b925999eff497f5f5b33bd05d73413bab9c6d
aaronz13248 <aaronz13248@gmail.com>
2022-06-01 18:13:07 -0400

    add music; NightSnow (calm), Vremya (hi-tech), VirtualLight (intimidating)

--- src/tetris/settings/GameSettings.java
+++ src/tetris/settings/GameSettings.java
@@ -1,4 +1,6 @@
 package tetris.settings;
 
 public class GameSettings {
+    public static int music; // 0 for no music, 1 for NightSnow, 2 for Vremya, 3 for VirtualLight
+    public static int volume; // self-explanatory
 }
