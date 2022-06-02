7911ea7a2cb131b0b4a6a04347e386c9b9686334
aaronz13248 <104523721+aaronz13248@users.noreply.github.com>
2022-06-01 20:13:18 -0400

    yes static yes file

--- src/tetris/settings/GameSettings.java
+++ src/tetris/settings/GameSettings.java
@@ -1,6 +1,7 @@
 package tetris.settings;
 
 //TODO: QUESTION: Should this be static? Should we save this in a file?
+//ANSWER: Static yes because these are essentially global variables, file yes because UsEr fRienDliNess
 public class GameSettings {
     public static int music; // 0 for no music, 1 for NightSnow, 2 for Vremya, 3 for VirtualLight
     public static int volume; // self-explanatory
