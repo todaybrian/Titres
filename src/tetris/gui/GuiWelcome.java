05be5e4b215c8c3aa20b6fccff572cab8786a395
Brian Yan <todaybryan@gmail.com>
2022-06-03 09:02:50 -0400

    Untie GuiWelcome to ticks

--- src/tetris/gui/GuiWelcome.java
+++ src/tetris/gui/GuiWelcome.java
@@ -7,21 +7,20 @@ import tetris.util.Assets;
 import javax.swing.*;
 
 public class GuiWelcome extends Gui {
-    //Number of render ticks that this will be displayed on the screen
-    private final int NUMBER_OF_TICKS = 180;
+    //Number of nanoseconds that this will be displayed on the screen
+    private final long NUMBER_OF_NS = 4 * 1000000000L;
 
     //Current number of render ticks that have passed, used to determine when to remove this from the screen
-    private int currentTick;
+    private long currentNS;
 
     public GuiWelcome(Gui parentScreen) {
         super(parentScreen);
-        currentTick = 0;
+        currentNS = System.nanoTime();
     }
 
     public void draw(GraphicsWrapper g){
-        if(currentTick < NUMBER_OF_TICKS){
-            currentTick++;
-            System.out.println(currentTick);
+        if(System.nanoTime() - currentNS < NUMBER_OF_NS){
+            currentNS++;
             ImageIcon welcomeScreen = new ImageIcon(Assets.WELCOME_SCREEN);
             g.drawImage(welcomeScreen.getImage(), 0, 0, GamePanel.INTERNAL_WIDTH, GamePanel.INTERNAL_HEIGHT);
         } else{
