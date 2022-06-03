3e715462457eebe3a6bb6bdf5657e6543e85954a
Brian Yan <todaybryan@gmail.com>
2022-06-03 09:58:46 -0400

    Add util

--- src/tetris/util/Util.java
+++ src/tetris/util/Util.java
@@ -1,5 +1,13 @@
 package tetris.util;
 
 public class Util {
+    private static double renderScale;
+    public static void setRenderScale(double renderScale){
+        Util.renderScale = renderScale;
+    }
+
+    private int scale(double val) {
+        return (int)Math.round(val * renderScale);
+    }
 
 }
