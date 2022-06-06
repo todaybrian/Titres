966aba1c6091c590ab8d8e87b5da2c4967dab4b9
aaronz13248 <aaronz13248@gmail.com>
2022-06-06 09:56:08 -0400

    randomizer structure

---
+++ src/tetris/game/randomizer/RandomizerSevenBag.java
@@ -0,0 +1,14 @@
+package tetris.game.randomizer;
+
+import java.awt.*;
+
+public class RandomizerSevenBag extends Randomizer{
+
+    public RandomizerSevenBag() {
+
+    }
+    Image getNext() {
+
+        return null;
+    }
+}
