763512ac986cc2b4b3e98c52686748cff6c17b2b
todaybrian <todaybryan@gmail.com>
2022-06-08 00:34:52 -0400

    Remove GraphicsConfiguration

--- src/tetris/GameFrame.java
+++ src/tetris/GameFrame.java
@@ -7,7 +7,7 @@ public class GameFrame extends JFrame {
     GamePanel panel;
     private GraphicsEnvironment graphicsEnvironment;
     private GraphicsDevice graphicsDevice;
-    private GraphicsConfiguration graphicsConfiguration;
+
     public DisplayMode displayMode;
     private int displayWidth, displayHeight;
     private int renderWidth, renderHeight;
@@ -17,7 +17,7 @@ public class GameFrame extends JFrame {
         // What does this do?
         graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
         graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
-        graphicsConfiguration = graphicsDevice.getDefaultConfiguration();
+
         displayMode = graphicsDevice.getDisplayMode();
 
         displayWidth = displayMode.getWidth();
