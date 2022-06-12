ebd4d6891569dba03a26036f4e29ee7ba339a8b2
aaronz13248 <aaronz13248@gmail.com>
2022-06-12 19:06:26 -0400

    fixed slider mouseover effect mismatch (and possibly the rounding error too?)

--- src/tetris/gui/widget/Slider.java
+++ src/tetris/gui/widget/Slider.java
@@ -9,19 +9,21 @@ import java.awt.*;
 public class Slider extends Button{
 
     protected int xPos;
+
+    protected double xSlider;
     protected int minValue, maxValue;
     protected Image icon;
     protected IPressable onChange;
 
     public Slider(int xPos, int yPos, int width, Image image, IPressable onChange, int minValue, int maxValue, int initValue) {
-        super((int)(((initValue-minValue+0.0)/(maxValue-minValue))*(width-image.getWidth(null)))+xPos, yPos, image, (click)->{});
+        super(xPos, yPos, image, (click)->{});
 
         this.xPos = xPos;
         this.width = width;
         this.icon = image;
         this.minValue = minValue;
         this.maxValue = maxValue;
-
+        this.xSlider = (((initValue-minValue+0.0)/(maxValue-minValue))*(width-image.getWidth(null)))+xPos;
         this.onChange = onChange;
     }
 
@@ -35,24 +37,24 @@ public class Slider extends Button{
         checkHover();
         if (isClicked()) {
             if (MouseInput.getLocation().getX()+icon.getWidth(null) > width+xPos) {
-                x = width+xPos -icon.getWidth(null);
+                xSlider = width+xPos -icon.getWidth(null);
             } else if (MouseInput.getLocation().getX() < xPos) {
-                x = xPos;
+                xSlider = xPos;
             } else {
-                x = (int) MouseInput.getLocation().getX() - icon.getWidth(null)/2;
+                xSlider = (int) MouseInput.getLocation().getX() - icon.getWidth(null)/2;
             }
             this.onChange.onChange(this);
         }
         g.setColor(new Color(0, 0, 0, 200));
         int offsetY = 20;
         g.fillRect(xPos, (int) (y+offsetY), width, height-2*offsetY);
-        g.drawImage(icon, (int) x, (int) y, icon.getWidth(null), icon.getHeight(null), null);
+        g.drawImage(icon, (int) xSlider, (int) y, icon.getWidth(null), icon.getHeight(null), null);
         g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1));
 
     }
 
     public double getValue() {
-        return ((x-xPos) / (width-icon.getWidth(null))) * (maxValue-minValue) + minValue;
+        return ((xSlider-xPos) / (width-icon.getWidth(null))) * (maxValue-minValue) + minValue;
     }
     public interface IPressable {
         void onChange(Slider onSlide);
