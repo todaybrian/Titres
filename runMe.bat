6654e15fd6f93ef435d7c141f58f6588bafb40f2
aaronz13248 <aaronz13248@gmail.com>
2022-06-15 08:54:19 -0400

    update runMe.bat

--- runMe.bat
+++ runMe.bat
@@ -1,2 +1,2 @@
-javac -d out/production/ics4u-assignment -target 1.8 -source 1.8 src/tetris/game/*.java src/tetris/controls/*.java src/tetris/gui/widget/*.java src/tetris/gui/*.java src/tetris/util/*.java src/tetris/settings/*.java src/tetris/*.java
+javac -d out/production/ics4u-assignment -target 1.8 -source 1.8 src/tetris/game/*.java src/tetris/game/randomizer/*.java src/tetris/controls/*.java src/tetris/gui/widget/*.java src/tetris/gui/*.java src/tetris/util/*.java src/tetris/settings/*.java src/tetris/*.java src/tetris/music/*.java
 java -cp out/production/ics4u-assignment tetris.Main -Dsun.java2d.uiScale=1.0
